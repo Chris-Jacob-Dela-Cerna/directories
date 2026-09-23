8a532168feb6961547d848208dcd697b91f0fa1e
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-09-23 20:31:08 +0800

    feat: add toggleStar method for directory

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 
-//  ---  Variables  ---
-
 const directories = [];
 
 
 
-//  
+function folder() {
+  
+}
 
-function directory(name, about=null, tags=[], primaryLanguage=null, lastUpdated=null) {
+function Directory(name, about=null, tags=[], primaryLanguage=null, lastUpdated=null) {
   if (!new.target) throw Error("cannot create object without [new].");
 
   this.name = name;
@@ -17,6 +17,11 @@ function directory(name, about=null, tags=[], primaryLanguage=null, lastUpdated=
   this.primaryLanguage = primaryLanguage;
   this.lastUpdated = lastUpdated;
   this.star = false;
+
+  this.toggleStar = function () {
+    if (this.star) this.star = false;
+    else this.star = true;
+  }
 }
 
-console.log(new directory("bugtopia", "Terminal game — a turn-based bug battle game built in Python.", ["game", "python", "cli", "oop", "terminal-game"], "PYTHON", "Jun 28, 2026"));
+const bugtopia = new Directory("bugtopia", "Terminal game — a turn-based bug battle game built in Python.", ["game", "python", "cli", "oop", "terminal-game"], "PYTHON", "Jun 28, 2026")
