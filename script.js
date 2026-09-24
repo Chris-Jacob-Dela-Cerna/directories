8a11136a1d4ee8fbb89d74dd4f3d3ed8ad323e8a
Chris-Jacob-Dela-Cerna <b1.tgr0509@gmail.com>
2026-09-24 20:38:46 +0800

    feat: add directory objects inside directories array for mock-up

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-const directories = [];
 
 
 
-function folder() {
-  
-}
-
-function Directory(name, about=null, tags=[], primaryLanguage=null, lastUpdated=null) {
+function Directory(name, about=null, tags=[], primaryLanguage=null) {
   if (!new.target) throw Error("cannot create object without [new].");
 
   this.name = name;
   this.about = about;
   this.tags = tags;
   this.primaryLanguage = primaryLanguage;
-  this.lastUpdated = lastUpdated;
   this.star = false;
 }
 
@@ -21,4 +15,12 @@ Directory.prototype.toggleStar = function () {
   this.star = !this.star;
 }
 
-const bugtopia = new Directory("bugtopia", "Terminal game — a turn-based bug battle game built in Python.", ["game", "python", "cli", "oop", "terminal-game"], "PYTHON", "Jun 28, 2026");
+const directories = [
+  new Directory("orion-sign-up", null, [], "HTML"),
+  new Directory("orion-dashboard", null, [], "HTML"),
+  new Directory("calculator", null, [], "JavaScript"),
+  new Directory("etch-a-sketch", null, [], "CSS"),
+  new Directory("landing-page", null, [], "CSS"),
+  new Directory("bugtopia", "Terminal game — a turn-based bug battle game built in Python.", ["game", "python", "cli", "oop", "terminal-game"], "PYTHON"),
+  new Directory("quizpin", "Terminal quiz app — create, run, and manage CSV-based quizzes from the command line.", ["python", "cli", "csv", "terminal"])
+];
