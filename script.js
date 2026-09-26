11fa0f6aa10b2ccbd56fcb8c095c096922f6450a
Chris Jacob Dela Cerna <b1.tgr0509@gmail.com>
2026-09-26 12:38:31 +0000

    reformat: organize JS code and variable/naming

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 
+//  ---  DOM References  ---
+
+const pageDirectories = document.getElementById("directories");
+
+
+
+//  ---  Directory Object Constructor  ---
+
 function Directory(name, about=null, tags=[], primaryLanguage=null) {
   if (!new.target) throw Error("cannot create object without [new].");
 
@@ -14,6 +22,10 @@ Directory.prototype.toggleStar = function () {
   this.star = !this.star;
 }
 
+
+
+//  ---  Mock-up Directories  ---
+
 const directories = [
   new Directory("orion-sign-up", null, [], "HTML"),
   new Directory("orion-dashboard", null, [], "HTML"),
@@ -24,39 +36,38 @@ const directories = [
   new Directory("quizpin", "Terminal quiz app — create, run, and manage CSV-based quizzes from the command line.", ["python", "cli", "csv", "terminal"], "PYTHON")
 ];
 
-const pageDirectories = document.getElementById("directories");
+
+
+//  ---  List of Directories  ---
 
 for (let i = 0; i < directories.length; i++) {
-  const currentDir = directories[i];
-  const dirContainer = document.createElement("div");
+  const currentDir = directories[i],
+        dirContainer = document.createElement("div");
 
-  const dirName = document.createElement("h3");
+  const dirName = document.createElement("h3"),
+        dirAbout = document.createElement("p"),
+        dirTags = document.createElement("ul"),
+        dirPrimaryLanguage = document.createElement("p");
+
   dirName.textContent = currentDir.name;
-
-  const dirAbout = document.createElement("p");
   dirAbout.textContent = currentDir.about;
-
-  const dirTags = document.createElement("ul");
-  const currentDirTags = currentDir.tags;
-  for (let ii = 0; ii < currentDirTags.length; ii++) {
-    const currentTag = currentDirTags[ii]
-    const dirTagContainer = document.createElement("div");
-
-    const dirTag = document.createElement("li");
-    const dirTagText = document.createElement("p");
+  //  ---  Directory Tags  ---
+  for (let ii = 0; ii < currentDir.tags.length; ii++) {
+    const currentTag = currentDir.tags[ii],
+          dirTagContainer = document.createElement("div"),
+          dirTag = document.createElement("li"),
+          dirTagText = document.createElement("p");
     dirTagText.textContent = currentTag;
 
     dirTag.appendChild(dirTagText);
     dirTagContainer.appendChild(dirTag);
     dirTags.appendChild(dirTagContainer);
   }
-
-  const dirPrimaryLang = document.createElement("p");
-  dirPrimaryLang.textContent = currentDir.primaryLanguage;
+  dirPrimaryLanguage.textContent = currentDir.primaryLanguage;
 
   dirContainer.appendChild(dirName);
   dirContainer.appendChild(dirAbout);
   dirContainer.appendChild(dirTags);
-  dirContainer.appendChild(dirPrimaryLang);
+  dirContainer.appendChild(dirPrimaryLanguage);
   pageDirectories.appendChild(dirContainer);
 }
